daa9c35c6b1f74ab949cdbc434b20802bf731026
TroyRWalker <trwalker@smu.edu>
2020-07-07 17:22:38 -0400

    Working to figure out bug

--- perfkitbenchmarker/linux_benchmarks/iperf_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/iperf_benchmark.py
@@ -110,15 +110,19 @@ def _RunIperf(sending_vm, receiving_vm, receiving_ip_address, thread_count, ip_t
                 FLAGS.iperf_runtime_in_seconds,
                 thread_count,
                 FLAGS.iperf_interval))
+  iperf_cmd = ('iperf -e --client %s --port %s --format m --time %s --interval s' %
+               (receiving_ip_address, IPERF_PORT,
+                FLAGS.iperf_runtime_in_seconds,
+                FLAGS.iperf_interval))
   # iperf_cmd = ('iperf -e --client %s --port %s --format m --time %s -P %s -i %s' %
   #              (receiving_ip_address, IPERF_PORT,
   #               FLAGS.iperf_runtime_in_seconds,
   #               thread_count,
   #               FLAGS.iperf_interval))
-  iperf_cmd = ('iperf -e --client %s --port %s --format m --time %s -P %s' %
-               (receiving_ip_address, IPERF_PORT,
-                FLAGS.iperf_runtime_in_seconds,
-                thread_count))
+  # iperf_cmd = ('iperf -e --client %s --port %s --format m --time %s -P %s' %
+  #              (receiving_ip_address, IPERF_PORT,
+  #               FLAGS.iperf_runtime_in_seconds,
+  #               thread_count))
   # the additional time on top of the iperf runtime is to account for the
   # time it takes for the iperf process to start and exit
   timeout_buffer = FLAGS.iperf_timeout or 30 + thread_count
