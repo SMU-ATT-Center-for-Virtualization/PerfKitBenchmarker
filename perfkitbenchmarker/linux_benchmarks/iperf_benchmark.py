32d20720f412162f6a5d6d387fecd012ebcbb18a
Mark Brubaker <mbrubaker@smu.edu>
2021-08-23 14:43:37 -0500

    beginning to fix some isses with regex

--- perfkitbenchmarker/linux_benchmarks/iperf_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/iperf_benchmark.py
@@ -168,11 +168,18 @@ def _RunIperf(sending_vm, receiving_vm, receiving_ip_address, thread_count,
 
     window_size_match = re.search(
         r'TCP window size: (?P<size>\d+\.?\d+) (?P<units>\S+)', stdout)
-    window_size = float(window_size_match.group('size'))
+    
+    window_size = 0
+    # search returns NoneType if no match is found
+    if window_size_match:
+        window_size = float(window_size_match.group('size'))
 
-    buffer_size = float(
-        re.search(r'Write buffer size: (?P<buffer_size>\d+\.\d+) \S+',
-                  stdout).group('buffer_size'))
+    buffer_size_match = re.search(r'Write buffer size: (?P<buffer_size>\d+\.\d+) \S+', stdout)
+
+    buffer_size = 0
+    # search returns NoneType if no match is found
+    if buffer_size_match:
+        buffer_size = float(buffer_size_match.group('buffer_size'))
 
     multi_thread = re.search((
         r'\[SUM\]\s+\d+\.\d+-\d+\.\d+\s\w+\s+(?P<transfer>\d+)\s\w+\s+(?P<throughput>\d+)'
